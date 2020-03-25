c9ffa01fd155d3b49470122f8b5980b049a38396
Grant Gilson <grantmaingilson@gmail.com>
2020-03-24 23:04:32 -0700

    added basic error handling for misinputed commands

--- cogs/nettools.py
+++ cogs/nettools.py
@@ -11,10 +11,28 @@ class NetTools(commands.Cog):
     async def on_ready(self):
         print(f"OH Bot connected")
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        """
+        Error event handler
+        Currently handling for err is None
+        @ctx: context object containing information about the caller
+        @err: error object raised by caller
+        Error types currently handled:
+        CommandNotFound,
+        """
+        sender = ctx.author._user
+        if isinstance(err, commands.CommandNotFound):
+            await ctx.send(
+                f"Command '{ctx.invoked_with}' does not exist\n"
+                f"{sender.mention} check your spelling"
+            )
+
     #Commands
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f"Pong! {round(self.client.latency * 1000)}")
+
 
 
 def setup(client):
