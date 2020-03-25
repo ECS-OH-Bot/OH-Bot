d848f5f311c8434c0f4c4ffa2ae1cef756641f60
Grant Gilson <grantmaingilson@gmail.com>
2020-03-24 21:57:20 -0700

    dictionary representation of user

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import logging
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -8,9 +9,17 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 client = commands.Bot(command_prefix=">")
 
-@client.event
-async def on_ready():
-    print(f"connected...")
+@client.command
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+@client.command
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
 
 if __name__ == '__main__':
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            client.load_extension(f"cogs.{filename[:-3]}")
     client.run(BOT_TOKEN)
+
--- cogs/OH_queue.py
+++ cogs/OH_queue.py
@@ -5,7 +5,30 @@ class OH_Queue(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self._OHQueue = dict()
 
+    @commands.command(aliases=["listqueue", "lsq"])
+    async def listQueue(self, ctx):
+        msg = ''
+        for _, name in self._OHQueue:
+            name = str(name)
+            name.join("\n")
+            msg.join(name)
+
+        print(msg)
+
+    @commands.command(aliases=["enterqueue", "eq"])
+    async def enterQueue(self, ctx: discord.ext.commands.context.Context):
+        sender = ctx.author
+        self._OHQueue[sender.id] = sender._user
+        await ctx.send(f"<@{sender.id}> you have been added to the queue")
+        # Call the list queue function to let them know where they stand in the queue
+        #self.listQueue(ctx)
+
+
+    @commands.command(aliases=['leavequeue', 'lq'])
+    async def leaveQueue(self, ctx):
+        pass
 
 
 def setup(client):
