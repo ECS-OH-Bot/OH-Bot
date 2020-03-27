c16eab10fe64a796745f00082b842a74c3e52c22
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2020-03-26 20:32:00 -0700

    Add queue channel id to main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import discord
 import os
-import logging
 from discord.ext import commands
 from dotenv import load_dotenv
 
 BOT_CHANNEL_ID=692585035609669632
+QUEUE_CHANNEL_ID=692894015565332521 # TODO: Move these to env?
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -30,4 +29,3 @@ if __name__ == '__main__':
         if filename.endswith(".py"):
             client.load_extension(f"cogs.{filename[:-3]}")
     client.run(BOT_TOKEN)
-
