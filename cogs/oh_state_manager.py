ad6831a0af52b07a2b9bb7aca08209a19b87ff51
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2020-03-29 17:04:10 -0700

    Added the ability for admins to open/close the queue

--- cogs/oh_state_manager.py
+++ cogs/oh_state_manager.py
@@ -4,40 +4,46 @@ Cog to manage opening / closing office hours
 from discord.ext.commands import Cog, Bot, Context
 from discord.ext import commands
 
-from cogs.user_utils import UserUtils
-
 from enum import Enum
+
+from user_utils import isAdmin
+from errors import OHStateError
+
 class OHState(Enum):
     CLOSED = 0
     OPEN = 1
 
+
+async def officeHoursAreClosed(context: Context) -> bool:
+    if not context.bot.get_cog("OHStateManager").state == OHState.CLOSED:
+        raise OHStateError("Office hours are not closed.")
+    return True
+
+async def officeHoursAreOpen(context: Context) -> bool:
+    if not context.bot.get_cog("OHStateManager").state == OHState.OPEN:
+        raise OHStateError("Office hours are not open.")
+    return True
+
 class OHStateManager(Cog):
-    state = OHState.CLOSED
-
-    def __init__(self, bot: Bot):
-        self.bot = Bot
-
-    @classmethod
-    def officeHoursAreClosed(cls) -> bool:
-        return OHStateManager.state == OHState.CLOSED
-
-    @classmethod
-    def officeHoursAreOpen(cls) -> bool:
-        return OHStateManager.state == OHState.OPEN
+    def __init__(self):
+        self.state: OHState = OHState.CLOSED
 
     @commands.command(aliases=["open", "start"])
-    #@commands.check(officeHoursAreClosed)
-    @commands.check(UserUtils.isAdmin)
+    @commands.check(officeHoursAreClosed)
+    @commands.check(isAdmin)
     async def startOH(self, context: Context):
         await context.author.send("Office hours have been opened. Students may now queue up")
         self.state = OHState.OPEN
+        # Trigger the queue message to reprint
+        await context.bot.get_cog("OH_Queue").onQueueUpdate()
 
     @commands.command(aliases=["close", "end"])
     @commands.check(officeHoursAreOpen)
-    @commands.check(UserUtils.isAdmin)
     async def stopOH(self, context: Context):
         await context.author.send("Office hours have been closed. No new students will be added to the queue.")
-        self.state = OHState.OPEN
+        self.state = OHState.CLOSED
+        # Trigger the queue message to reprint
+        await context.bot.get_cog("OH_Queue").onQueueUpdate()
 
 def setup(bot: Bot):
-    bot.add_cog(OHStateManager(bot))
+    bot.add_cog(OHStateManager())
