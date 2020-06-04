20f92d1c4ad5340fc6c99fa448282afb9c04eb86
Grant Gilson <grantmaingilson@gmail.com>
2020-06-04 00:25:04 -0700

    added function to verify deps are installed on system

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+DEPS=("python3")
+
 main(){
+    for dep in "${DEPS[@]}"
+    do
+        verifyDep "$dep"
+    done
+
     setupVenv
 }
 
@@ -9,11 +16,31 @@ setupVenv(){
     python3 -m venv ./venv
     . ./venv/bin/activate
    
-    if [[ which python3 == "foo" ]]; then
+    INTERPRETER=$(command -v python3)
+
+    if [[ $INTERPRETER != *"/OH-Bot/venv"* ]]; then
         echo "Failed to activate virtual environment"
+        exit 129
     else
-        echo "foo"
+        echo "Virtual Enironment Activated"
     fi
+
+    # Install the deps after we have verified that the venv is loaded
+    python3 -m pip install -r requirements.txt
 }
+
+verifyDep(){
+
+    if ! command -V "$1" > '\dev\null' 2>&1; then
+        echo "Dependency $1 not met" 
+        exit 128
+    fi
+    return 0
+}
+
+generateEnv(){
+    
+}
+
 
 main
